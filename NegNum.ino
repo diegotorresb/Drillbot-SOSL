79ee7594434fef7da6998653b68b79639d8dfe79
IsaacPimienta <148139212+IsaacPimienta@users.noreply.github.com>
2023-10-30 09:48:17 -0700

    Update NegNum.ino

--- NegNum.ino
+++ NegNum.ino
@@ -22,7 +22,7 @@ void setup() { // Black Green Red Blue (A+ A- B+ B-)
     stepper2.setAcceleration(40);
     Serial.begin(9600);
     limitSwitch.setDebounceTime(50);
-    Serial.println("<Arduino is ready>, Enter number, example: <100> for 100");
+    Serial.println("<Arduino is ready>, Enter number, example: <100> for 100"); // ignore this 
 }
 
 void loop() {
@@ -37,13 +37,13 @@ void recvStep() { //this function reads the value given through matlab, the star
     char endMarker = '>';
     char rc;
 
-    while (Serial.available() > 0 && charRead == true){
+    while (Serial.available() > 0 && charRead == true){// this loop takes in the stepper motor name, x or y
       motorName = Serial.read();
       charRead = false;
       newData = false;
     }
  
-    while (Serial.available() > 0 && newData == false) {
+    while (Serial.available() > 0 && newData == false) {//this part takes in the value that the motor will run to, + or -
         rc = Serial.read();
 
         if (recvInProgress == true) {
@@ -61,7 +61,7 @@ void recvStep() { //this function reads the value given through matlab, the star
                 newData = true;
                 charRead = true;
                 newNum = atoi(receivedChars); // TEST
-                stepTime = true; //TEST
+                stepTime = true; //TEST    **it seems like these tests were successful
                // TEST newerData = true;
             }
         }
@@ -72,7 +72,7 @@ void recvStep() { //this function reads the value given through matlab, the star
     }
 }
 
-void runStepper(){
+void runStepper(){//this function runs the stepper  motor, the limit switch function, when active, will stop the movement
   if (motorName == 'x'){
     while (stepTime == true){
       stepper.moveTo(newNum);
@@ -95,12 +95,12 @@ void runStepper(){
   }
 }
 
-void limmySwitch() {
-/*  limitSwitch.loop();
-  
+void limmySwitch() {//this is currently commented out and a bit buggy, but the gist is to have the arduino constantly read 
+    /*  limitSwitch.loop();//the state of the limit switch, either open or closed, and when it changes, the code gets stuck
+  //in the loop and cannot continue to move the steppers with stepper.run(). There is probably a better way to do this
   if (limitSwitch.getState() == HIGH){
     limmy = true;
-    while (limmy == true){
+    while (limmy == true){//this while loop infinitely traps the code
       //find way to make steppers stop and return to zero?
     }
   }*/
