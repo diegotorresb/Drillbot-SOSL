d1fe8678ec737772572165afe733a1d975c2f168
IsaacPimienta <148139212+IsaacPimienta@users.noreply.github.com>
2023-10-30 09:34:56 -0700

    Add files via upload

---
+++ NegNum.m
@@ -0,0 +1,20 @@
+%MATLAB Code for Serial Communication with Arduinofclose(instrfind);
+delete(instrfind);
+keep = true;
+x=serialport('COM8', 9600);
+fopen(x);
+pause(1);
+while (keep == true)
+    a = input('Enter "e" to exit, Enter x or y:', "s");
+    if (a == 'e')
+        break;
+    end
+    fprintf(x, '%c', a);
+    b = input("Enter the number of revs (100 = 90 deg)? Ex: 100 -> <100>, enter 'exit' to exit ", "s");
+    if (b == "exit")
+        break;
+    end
+    fwrite(x, b);
+    pause(0.1);
+end
+fclose(x);
